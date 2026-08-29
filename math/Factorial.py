fea5fb6cce94ac9815b4b9400abd55d4e7916449
Mitsuharu-YAMAURA <251524124+Mitsuharu-YAMAURA@users.noreply.github.com>
2026-08-30 06:38:49 +0900

    update Factorial

--- math/Factorial.py
+++ math/Factorial.py
@@ -1,10 +1,20 @@
 # https://github.com/Mitsuharu-YAMAURA/competitive-programming-library
 
 
-def Factorial(n, mod=998244353):
+# 特定の階乗を計算
+def factorial(n, mod=998244353):
     fact = 1
-    for i in range(1, n):
-        fact *= i
-        fact %= mod
+
+    for i in range(1, n + 1):
+        fact = fact * i % mod
+
+    return fact
+
+
+# 1からnまでの階乗をすべて前計算
+def factorial_table(n, mod=998244353):
+    fact = [1] * (n + 1)
+    for i in range(2, n + 1):
+        fact[i] = fact[i - 1] * i % mod
 
     return fact
