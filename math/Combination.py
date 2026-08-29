fdb90c3cd1073431c1b1def7d3787990b0fb1214
Mitsuharu-YAMAURA <251524124+Mitsuharu-YAMAURA@users.noreply.github.com>
2026-08-30 06:36:33 +0900

    update Combination

--- math/Combination.py
+++ math/Combination.py
@@ -1,5 +1,13 @@
 # https://github.com/Mitsuharu-YAMAURA/competitive-programming-library
-def Combination(n, k, mod=998244353):
+
+
+# modが素数でk<modの場合
+def combination(n, k, mod=998244353):
+    if k < 0 or n < k:
+        return 0
+
+    k = min(k, n - k)
+
     top = 1
     bottom = 1
 
@@ -12,3 +20,25 @@ def Combination(n, k, mod=998244353):
 
     nCk = top * pow(bottom, -1, mod)
     return nCk % mod
+
+
+def combination_table(n, mod=998244353):
+    fact = [1] * (n + 1)
+    inv_fact = [1] * (n + 1)
+
+    for i in range(1, n + 1):
+        fact[i] = fact[i - 1] * i % mod
+
+    inv_fact[n] = pow(fact[n], -1, mod)
+    for i in range(n, 0, -1):
+        inv_fact[i - 1] = (
+            inv_fact[i] * i % mod
+        )  # ((i-1)!)^(-1)=(i!)^(-1)*iだからかけるだけでいい
+
+    return fact, inv_fact
+
+
+def calc_combination(n, k, fact, inv_fact, mod=998244353):
+    if k < 0 or k > n:
+        return 0
+    return fact[n] * inv_fact[k] % mod * inv_fact[n - k] % mod
